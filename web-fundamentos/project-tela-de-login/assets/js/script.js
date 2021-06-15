1ab360cab2a7535da50b2558d5921490d5693aa0
unknown <manuogg90@gmail.com>
2021-06-15 14:36:43 -0300

    feat: var, let e const no js

--- web-fundamentos/project-tela-de-login/assets/js/script.js
+++ web-fundamentos/project-tela-de-login/assets/js/script.js
@@ -1,2 +1,6 @@
-alert("Shannaro!!!");
-console.log("shannaro!!!");
+let slogan = "Hello word!";
+
+slogan = "Shannaro";
+
+console.log("O slogan da Sakura é: " + slogan);
+console.log(`O slogan da Sakura é: ${slogan}`); // usando crase + ${} outra forma de fazer concatenação// 
