9a1ffe50d29a063ebc07b69615aacfc6e548a692
unknown <manuogg90@gmail.com>
2021-06-15 15:26:05 -0300

    feat: eventListener

--- web-fundamentos/project-tela-de-login/assets/js/script.js
+++ web-fundamentos/project-tela-de-login/assets/js/script.js
@@ -4,3 +4,31 @@ slogan = "Shannaro";
 
 console.log("O slogan da Sakura é: " + slogan);
 console.log(`O slogan da Sakura é: ${slogan}`); // usando crase + ${} outra forma de fazer concatenação// 
+
+// e => chamar uma função "simples";
+// addEventListener escutar a ação do usuário.
+document.getElementById('btn-submit').addEventListener('click', e => {
+
+  console.log('o botão foi clicado');
+
+});
+
+document.getElementById('form-login').addEventListener('mouseenter', e => {
+
+  console.log('o mouse está sobre o formulário');
+
+});
+
+document.querySelector('#form-login').addEventListener('mouseleave', e => {
+
+  console.log('o mouse está fora do formulário');
+
+});
+
+document.querySelector('#form-login').addEventListener('submit', e => {
+
+  e.preventDefault(); //evento para cancelar comportamento pradão do navegador
+
+  console.log('Formulário enviado! Aqui vai o Ajax.')
+
+});
